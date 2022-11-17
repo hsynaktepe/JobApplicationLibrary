cdf8e420411db608e5d83bfcbbc3acf1401f4618
hsynaktepe <56879020+hsynaktepe@users.noreply.github.com>
2022-11-17 15:23:26 +0300

    Add project files.

---
+++ JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -0,0 +1,21 @@
+namespace JobApplicationLibrary.UnitTest
+{
+    public class ApplicationEvaluateUnitTest
+    {
+       
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+            
+        }
+         [Test]
+        public void Test2()
+        {
+            Assert.Fail("This test failed");
+            
+        }
+
+    }
+}
---
+++ JobApplicationLibrary/ApplicationEvaluator.cs
@@ -0,0 +1,23 @@
+﻿using JobApplicationLibrary.Models;
+
+namespace JobApplicationLibrary
+{
+    public class ApplicationEvaluator
+    {
+        public ApplicationResult Evaluate(JobApplication form)
+        {
+            return ApplicationResult.AutoAccepted;
+        }
+    }
+
+    public enum ApplicationResult
+    {
+        AutoRejected,
+        TransferredToHR,
+        TransferredToLead,
+        TransferredToCTO,
+        AutoAccepted
+
+    }
+
+}
