0dd296a49bdd868e5124f5069f3facca4409855b
hsynaktepe <56879020+hsynaktepe@users.noreply.github.com>
2022-11-21 17:27:12 +0300

    Added dependency

--- JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
+++ JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -28,6 +28,51 @@ namespace JobApplicationLibrary_UnitTest
             var appResult = evaluator.Evaluate(form);
 
             //Assert
+            Assert.AreEqual(ApplicationResult.AutoRejected, appResult);
+            //Assert.AreEqual(ApplicationResult.AutoAccepted, appResult);
+
+        }
+
+       
+         [Test]
+        public void Application_WithNoStack_TransferredToAutoRejected()
+        {
+            //Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant() { Age = 19},
+                TechStackList = new System.Collections.Generic.List<string>() { ""}
+                
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(form);
+
+            //Assert
+            //Assert.AreEqual(ApplicationResult.AutoRejected, appResult);
+            Assert.AreEqual(ApplicationResult.AutoRejected, appResult);
+
+        }
+
+        [Test]
+        public void Application_WithStackOver75P_TransferredToAutoAccepted()
+        {
+            //Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant() { Age = 19 },
+                TechStackList = new System.Collections.Generic.List<string>() 
+                { "c#", "Rabbitmq", "microsergvice", "visual studuio" },
+                YearsOfExperience = 16
+
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(form);
+
+            //Assert
             //Assert.AreEqual(ApplicationResult.AutoRejected, appResult);
             Assert.AreEqual(ApplicationResult.AutoAccepted, appResult);
 
--- JobApplicationLibrary/ApplicationEvaluator.cs
+++ JobApplicationLibrary/ApplicationEvaluator.cs
@@ -1,20 +1,46 @@
 ﻿using JobApplicationLibrary.Models;
+using JobApplicationLibrary.Services;
 
 namespace JobApplicationLibrary
 {
-    
+
     public class ApplicationEvaluator
     {
         private const int minAge = 18;
+        private const int autoAcceptedYearOfExperience = 15;
+        private List<string> techStackList = new() { "c#", "Rabbitmq", "microsergvice", "visual studuio" };
+        private IdentityValidator IdentityValidator;
+
+        public ApplicationEvaluator()
+        {
+            IdentityValidator = new IdentityValidator();
+        }
+
         public ApplicationResult Evaluate(JobApplication form)
         {
             if (form.Applicant.Age < minAge)
                 return ApplicationResult.AutoRejected;
 
+            var sr = GetTechStackSimilarityRate(form.TechStackList);
+
+            if (sr < 25)
+                return ApplicationResult.AutoRejected;
+
+            if (sr > 75 && form.YearsOfExperience >= autoAcceptedYearOfExperience)
+                return ApplicationResult.AutoAccepted;
+
+
             return ApplicationResult.AutoAccepted;
         }
+
+        private int GetTechStackSimilarityRate(List<string> techStacks)
+        {
+            var matchedCount = techStacks.Where(i => techStackList.Contains(i, StringComparer.OrdinalIgnoreCase)).Count();
+
+            return (int)((double)matchedCount / techStackList.Count) * 100;
+        }
     }
-  
+
 
     public enum ApplicationResult
     {
